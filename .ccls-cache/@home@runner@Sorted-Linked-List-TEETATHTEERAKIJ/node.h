73871a4668da59b1c5dc44d4dc0a8471f5842645
ingT6481221 <132637600+ingT6481221@users.noreply.github.com>
2023-07-04 08:08:19 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Sorted-Linked-List-TEETATHTEERAKIJ/node.h
@@ -0,0 +1,53 @@
+// self-referential structure
+#include <iomanip>
+class Node {
+private:
+  int value;
+
+  Node *nextPtr;
+  Node *pPtr; // for doubly linked list
+public:
+  Node(int x = 0);
+  ~Node();
+  Node* get_next();
+  Node* get_prev();
+  int get_data();
+  void set_next(Node *t);
+  void set_prev(Node *t);
+  
+  void print();
+
+  // for doubly linked list
+  /*
+  Node * get_prev();
+  void set_prev(Node* t);
+  */
+};
+
+typedef Node *NodePtr; // synonym for Node*
+
+Node::Node(int x) {
+  value = x;
+  nextPtr = NULL;
+  pPtr=NULL;
+}
+
+Node::~Node() { cout << value << " deleted" << endl; }
+
+NodePtr Node::get_next() { return nextPtr; }
+
+
+int Node::get_data() { return value; }
+
+void Node::set_next(NodePtr t) {
+  this->nextPtr = t; 
+}
+
+void Node::print(){
+  cout << setw(3) << value;
+}
+void Node::set_prev(NodePtr t){
+  pPtr=t;
+}
+NodePtr Node::get_prev(){return pPtr;}
+//comment
